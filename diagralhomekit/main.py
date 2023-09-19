03230456a5eef90430a8d82ff37b33e4832ae5e9
d9pouces <github@19pouces.net>
2023-09-19 15:42:57 +0200

    feat: centralization of the log config

--- diagralhomekit/main.py
+++ diagralhomekit/main.py
@@ -5,17 +5,11 @@
 # ##############################################################################
 """All the main functions."""
 import argparse
-import logging
 import os
 import pathlib
 import signal
-import socket
-import sys
-import urllib.parse
-from multiprocessing import Queue
 
-import sentry_sdk
-from logging_loki import LokiQueueHandler, emitter
+import systemlogger
 
 # noinspection PyPackageRequirements
 from pyhap.accessory import Bridge
@@ -26,7 +20,7 @@ from pyhap.accessory_driver import AccessoryDriver
 from diagralhomekit.config import HomekitConfig
 from diagralhomekit.diagral import DiagralHomekitPlugin
 
-logger = logging.getLogger("diagralhomekit")
+logger = systemlogger.getLogger("diagralhomekit")
 
 
 def main():
@@ -34,8 +28,6 @@ def main():
     parser = argparse.ArgumentParser()
     default_port = int(os.environ.get("DIAGRAL_PORT", "51826"))
     default_config_dir = os.environ.get("DIAGRAL_CONFIG", "/etc/diagralhomekit")
-    default_sentry_dsn = os.environ.get("DIAGRAL_SENTRY_DSN")
-    default_loki_url = os.environ.get("DIAGRAL_LOKI_URL")
     verbosity = int(os.environ.get("DIAGRAL_VERBOSITY", 0))
     parser.add_argument(
         "--create-config",
@@ -49,8 +41,6 @@ def main():
         default=pathlib.Path(default_config_dir),
         type=pathlib.Path,
     )
-    parser.add_argument("--sentry-dsn", default=default_sentry_dsn)
-    parser.add_argument("--loki-url", default=default_loki_url)
     parser.add_argument("-v", "--verbosity", default=verbosity, type=int)
     args = parser.parse_args()
     config_dir = args.config_dir
@@ -64,48 +54,7 @@ def main():
         print(content)
         print("EOF")
         return
-
-    handler = logging.StreamHandler(sys.stdout)
-    if args.verbosity == 0:
-        logger.setLevel(logging.WARNING)
-    elif args.verbosity == 1:
-        logger.setLevel(logging.INFO)
-    else:
-        logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
     listen_port = args.port
-
-    if args.loki_url:
-        try:
-            hostname = socket.gethostname()
-        except socket.gaierror:
-            hostname = "localhost"
-        emitter.LokiEmitter.level_tag = "level"
-        parsed_url = urllib.parse.urlparse(args.loki_url)
-        url = f"{parsed_url.scheme}://{parsed_url.hostname}"
-        if parsed_url.port:
-            url += f":{parsed_url.port}"
-        url += parsed_url.path
-        if parsed_url.query:
-            url += f"?{parsed_url.query}"
-        handler = LokiQueueHandler(
-            Queue(-1),
-            url=url,
-            tags={
-                "application": "diagralhomekit",
-                "log_source": "diagralhomekit",
-                "hostname": hostname,
-            },
-            auth=(parsed_url.username or "", parsed_url.password or ""),
-            version="1",
-        )
-        logger.addHandler(handler)
-        logger.debug("Loki configured.")
-
-    if args.sentry_dsn:
-        sentry_sdk.init(args.sentry_dsn)
-        logger.debug("Sentry DSN configured.")
-
     run_daemons(
         config_dir,
         listen_port,
